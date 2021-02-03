c6161da1bb39b817bea3b013144a4125e695d894
ykykyk112 <ykykyk112@naver.com>
2021-02-03 23:52:23 +0900

    2021.02.03 다이나믹 프로그래밍 공부

--- 이코테_by_Python/DP_4.py
+++ 이코테_by_Python/DP_4.py
@@ -1,15 +1,18 @@
 n, m = map(int, input().split())
 coin = []
-count = [-1] * (m+1)
+count = [10001] * (m+1)
+count[0] = 0
 
 # input all coin
 for i in range(n) :
     coin.append(int(input()))
 
-for i in range(m+1) :
-    compare = [0] * n
-    for j in coin :
-        if i % j == 0 :
-           mininum = min(count) 
-        else :
-            count[i] = -1
+for i in range(n) :
+    for j in range(coin[i], m+1) :
+        if count[j-coin[i]] != 10001 :
+            count[j] = min(count[j], count[j-coin[i]] + 1)
+
+if count[m] == 10001 :
+    print(-1)
+else :
+    print(count[m])
